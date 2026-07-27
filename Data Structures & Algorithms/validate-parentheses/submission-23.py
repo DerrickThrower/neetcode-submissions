a38fc01fee872858f023635d60535ae4e6ca29c7
DerrickThrower <75774890+DerrickThrower@users.noreply.github.com>
2026-07-27 08:44:45 -0700

    Add: validate-parentheses - submission-23

---
+++ Data Structures & Algorithms/validate-parentheses/submission-23.py
@@ -0,0 +1,35 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        stack = []
+        closing = {
+            "}":"{",
+            "]":"[",
+            ")":"("
+        }
+
+
+        for char in s:
+
+            if char in closing:
+
+                if stack and stack[-1] == closing[char]:
+                    stack.pop()
+                else:
+                    return False
+    
+            else:
+                stack.append(char)
+
+
+        if stack:
+            return False
+
+        return True
+
+
+
+
+
+        
+        
